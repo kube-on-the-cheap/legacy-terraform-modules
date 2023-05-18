5a9f98dbadac9c29fd1c3e26eb4c5984f3a77606
Marco Bulgarini <marco.bulgarini@gmail.com>
2023-05-18 22:57:01 +0200

    feat(vcn): Change the subnet allocation method
    Signed-off-by: Marco Bulgarini <marco.bulgarini@gmail.com>

--- modules/oci-networking/networks.tf
+++ modules/oci-networking/networks.tf
@@ -1,3 +1,21 @@
+
+module "subnet_addrs" {
+  source  = "hashicorp/subnets/cidr"
+  version = "1.0.0"
+
+  base_cidr_block = var.oci_networks_vcn_cidr
+  networks = concat(
+    [
+      {
+        name     = "regional"
+        new_bits = 4
+      }
+    ],
+    [for name, id in var.oci_availability_domains : { "name" = name, "new_bits" = 6 }]
+  )
+}
+
+
 resource "oci_core_vcn" "vcn" {
   compartment_id = var.oci_compartment_id
 
@@ -10,7 +28,7 @@ resource "oci_core_subnet" "vcn_regional_subnet" {
   compartment_id = var.oci_compartment_id
 
   vcn_id            = oci_core_vcn.vcn.id
-  cidr_block        = cidrsubnet(one(oci_core_vcn.vcn.cidr_blocks), 4, 0)
+  cidr_block        = module.subnet_addrs.network_cidr_blocks["regional"]
   display_name      = format("%s-%s-regional-subnet", data.oci_identity_compartment.compartment.name, var.oci_region)
   security_list_ids = [oci_core_vcn.vcn.default_security_list_id]
 
@@ -22,7 +40,7 @@ resource "oci_core_subnet" "vcn_ad_subnets" {
   compartment_id = var.oci_compartment_id
 
   vcn_id            = oci_core_vcn.vcn.id
-  cidr_block        = cidrsubnet(one(oci_core_vcn.vcn.cidr_blocks), 4, index(keys(var.oci_availability_domains), each.key) + 1)
+  cidr_block        = module.subnet_addrs.network_cidr_blocks[each.key]
   display_name      = format("%s-%s-subnet", data.oci_identity_compartment.compartment.name, lower(split(":", each.key)[1]))
   security_list_ids = [oci_core_vcn.vcn.default_security_list_id]
 }
