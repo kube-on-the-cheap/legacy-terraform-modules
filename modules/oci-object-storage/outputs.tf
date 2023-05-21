84a9023d88f246a40ebac6cc5ac7119c90cd1c84
Marco Bulgarini <marco.bulgarini@gmail.com>
2023-05-21 09:54:17 +0200

    fix(object-storage): Fix output rename
    Signed-off-by: Marco Bulgarini <marco.bulgarini@gmail.com>

--- modules/oci-object-storage/outputs.tf
+++ modules/oci-object-storage/outputs.tf
@@ -2,9 +2,7 @@ output "s3_credentials" {
   sensitive   = true
   description = "S3 Compatibility Layer credentials"
   value = var.bucket_create_s3_access_key ? {
-    (var.bucket_name) : {
-      "ACCESS_KEY" = one(oci_identity_customer_secret_key.bucket_secret_key[*].id)
-      "SECRET_KEY" = one(oci_identity_customer_secret_key.bucket_secret_key[*].key)
-    }
+    "ACCESS_KEY" = one(oci_identity_customer_secret_key.bucket_secret_key[*].id)
+    "SECRET_KEY" = one(oci_identity_customer_secret_key.bucket_secret_key[*].key)
   } : null
 }
